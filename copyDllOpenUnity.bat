c85ede71605b77d713fceb1cddf043f4e26b45b3
James Owen <jamesowen012@gmail.com>
2017-08-03 19:49:27 +1000

    Updated DLL's and it now builds in release mode instead of debug

--- copyDllOpenUnity.bat
+++ copyDllOpenUnity.bat
@@ -15,9 +15,9 @@ taskkill /IM unity.exe
 
 echo building dll'd
 echo building 32 Bit project
-devenv %PROJDIR% /build "Debug|x86"
+devenv %PROJDIR% /build "Release|x86"
 echo building 64 Bit project
-devenv %PROJDIR% /build "Debug|x64"
+devenv %PROJDIR% /build "Release|x64"
 
 echo copying dll's
 copy %DLLDIR%\Win32\ControllerInputDll.dll "%STARTDIR%\Unity\Assets\JInput\Plugins\DirectInput\32"
